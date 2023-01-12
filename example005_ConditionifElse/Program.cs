cbd8497216bec8d980c7b49ec6e9ef89089ea07b
sasergei <soundboy@inbox.ru>
2023-01-13 00:28:34 +0300

    add example 005 ConditionifElse

---
+++ example005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿string username = Console.ReadLine();
+if (
+    username.ToLower() == "маша"
+)
+{
+    Console.WriteLine("Ура это же Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}

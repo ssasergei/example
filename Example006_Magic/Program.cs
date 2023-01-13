9d83f1ea689dab7f89331a48460c75a163aa6ea8
sasergei <soundboy@inbox.ru>
2023-01-13 19:34:29 +0300

    add example 007 magic good

--- Example006_Magic/Program.cs
+++ Example006_Magic/Program.cs
@@ -16,23 +16,23 @@ Console.WriteLine("+");
 int x = xa, y = xb;
 
 int count = 0;
-while (count < 100);
+while (count < 100)
 
 {
     int what = new Random().Next (0, 3); // 0 1 2
-    if (what == 0);
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
     
-    if (what == 1);
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
     
-    if (what == 2);
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
